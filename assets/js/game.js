23c202aaee263c3cd4b06ec1c1d3126e990b2d75
Xan <tsadiktalmudim@gmail.com>
2022-02-14 20:17:38 -0600

    added health values and attack principles

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,39 @@
-window.alert("This is stuff!")
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!")
+    } else {
+        window.alert(enemyName + " stil has " + enemyHealth + " health left.");
+    }
+    playerHealth = playerHealth - enemyAttack
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaning."
+    )
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+};
+if (playerHealth > 0) {
+    console.log("Your player is still alive!")
+};
+
+fight();
